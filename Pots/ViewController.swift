3a51b9be4bc40a68b671c27a2a0eaf5100365786
Ryan <ryekoda@gmail.com>
2018-04-07 02:06:37 -0700

    Initial Commit

---
+++ Pots/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pots
+//
+//  Created by Ryan on 4/7/18.
+//  Copyright © 2018 pots. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
